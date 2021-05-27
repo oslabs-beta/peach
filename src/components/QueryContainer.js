42b2d1ed065c9d9100cc23793461a2c88f8aa5de
Alura <aluracm@MacBook-Pro.local>
2021-05-27 10:31:17 -0500

    started hooking up user queries to relay

--- src/components/QueryContainer.js
+++ src/components/QueryContainer.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
+// look into useLazyQueryLoader
+
+// figure out how to make the below line dynamic (depending on the query)
+// import type {HomeTabQuery} from 'HomeTabQuery.graphql';
+const {graphql, usePreloadedQuery} = require('react-relay');
+
 
 const QueryContainer = (props) => {
   const [query, setQuery] = useState('');
@@ -11,8 +17,28 @@ const QueryContainer = (props) => {
   }
 
   const submitQuery = () => {
-    //perform GraphQl query with text contained in query state.
-    //update contents of ResponseDisplay with returned value of query.
+    // perform GraphQl query with text contained in query state.
+    // update contents of ResponseDisplay with returned value of query.
+    // change HomeTabQuery to be query name
+    type Props = {
+      queryRef: PreloadedQuery<HomeTabQuery>,
+    };
+    function HomeTab(props: Props) {
+      const data = usePreloadedQuery<HomeTabQuery>(
+        graphql`
+          query HomeTabQuery($id: ID!) {
+            user(id: $id) {
+              name
+            }
+          }
+        `,
+        props.queryRef,
+      );
+    
+      return (
+        <h1>{data.user?.name}</h1>
+      );
+    }
   }
   
   return (
