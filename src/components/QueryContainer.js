41bd64620736b243bb7f09757904db6724d4b6da
Graham Albachten <albachteng@gmail.com>
2021-06-01 17:10:28 -0500

    clean up and comment more components, delete a few components that were leftover from earlier experiments

--- src/components/QueryContainer.js
+++ src/components/QueryContainer.js
@@ -1,3 +1,8 @@
+/* 
+stateful component updates query string to be run in App.js via fs module,
+also reruns relay compiler when the query is submitted
+*/
+
 import React, { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
@@ -7,15 +12,8 @@ import importedQuery from '../relay/importedQuery';
 import '../styles/styles.css'
 import aliasID from '../relay/aliasID';
 
-// look into useLazyQueryLoader
-
-// figure out how to make the below line dynamic (depending on the query)
-// import type {HomeTabQuery} from 'HomeTabQuery.graphql';
-const {graphql, usePreloadedQuery} = require('react-relay');
-
 //require in exec to run terminal commands in js:
 const execSync = require('child_process').execSync;
-
 
 const QueryContainer = () => {
   let initialQueryText = importedQuery.params.text;
@@ -28,11 +26,9 @@ const QueryContainer = () => {
   const submitQuery = () => {
     const queryFileStart = 'import graphql from \'graphql\'\;\nexport default graphql`';
     const queryFileEnd = '`;';
-    
     const fullQueryText = aliasID(queryFileStart + queryText + queryFileEnd);
     fs.writeFileSync(path.resolve('./src/relay/importedQuery.js'), fullQueryText);
     
-
     const output = execSync('npm run relay', { encoding: 'utf-8' });
     console.log('Output was:\n', output);
   }
--- src/components/ResponseDisplay.js
+++ src/components/ResponseDisplay.js
@@ -1,16 +1,19 @@
-import React, { useState, useEffect } from 'react';
+/* 
+stateless component renders query response data passed from App.js 
+*/ 
+
+import React from 'react';
 import Container from 'react-bootstrap/Container';
 import '../styles/styles.css'
 
 
 const ResponseDisplay = (props) => {
 
-  //Stringifies result and removes double quotes.
+  //Stringifies result and removes double quotes and 
   const format = (response) => {
     const output = JSON.stringify(response, null, 2);
     return output.replace(/"/g, '');
   }
-  
 
   return (
     <Container className='response-container'>
