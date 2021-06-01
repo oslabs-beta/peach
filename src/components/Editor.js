f865784ce3ac00a1c71ec1cbccc0346b73e92e17
Alura <aluracm@MacBook-Pro.local>
2021-06-01 16:49:38 -0500

    moving functionality from editor.js to VariableInput component

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -7,7 +7,7 @@ export default function Editor(props) {
     const { language, value, onChange } = props;
 
     const handleChange = (editor, data, value) => {
-        onChange(value)
+        onChange(value);
     }
     return (
         <div className='editor-container'>
@@ -26,4 +26,4 @@ export default function Editor(props) {
 
         </div>
     )
-}
+};
--- src/components/VariableInput.jsx
+++ src/components/VariableInput.jsx
@@ -2,12 +2,32 @@ import React, { useState } from 'react';
 import '../styles/styles.css';
 import Editor from './Editor';
 
+//importing library for code editor
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/mode/javascript/javascript';
+import { Controlled as ControlledEditor } from 'react-codemirror2';
+
 const VariableInput = props => {
     const [state, setState] = useState('');
+
+    const handleChange = (editor, data, value) => {
+        onChange(value);
+    }
 
     return (
         <div>
             <h1>Variable Input</h1>
+            <ControlledEditor
+            onBeforeChange={handleChange}
+            value={value}
+            className='code-mirror-wrapper'
+            options={{
+                lineWrapping: true,
+                lint: true,
+                mode: language,
+                lineNumbers: true,
+            }}
+            />
             <Editor
             language='javascript'
             value = {state}
