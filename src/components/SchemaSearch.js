e53d37edc1d7f4c83d0d31adddc02464eb533868
Kall_MeKami <hopefelixn@gmail.com>
2021-06-01 17:09:39 -0400

    Update SchemaSearch.js

--- src/components/SchemaSearch.js
+++ src/components/SchemaSearch.js
@@ -2,23 +2,65 @@ import React, { useState } from 'react';
 
 
 const SchemaSearch = props => {
-    const [state, setState] = useState('');
-    const handleClick = (e) => {
-      e.persist()
-      setState = e.target.value
-      return
+  const items = [
+    {
+      id: 0,
+      name: 'Cobol'
+    },
+    {
+      id: 1,
+      name: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Basic'
+    },
+    {
+      id: 3,
+      name: 'PHP'
+    },
+    {
+      id: 4,
+      name: 'Java'
     }
-    return (
-        <div>
-          <h1>Schema SchemaSearch</h1>
-          <textarea 
-            value={state}
-            onChange={setState}
-            className='schemaSearch'
-            ></textarea>
-          <button onClick={handleClick}></button>
+  ]
+
+  const handleOnSearch = (string, results) => {
+    // onSearch will have as the first callback parameter
+    // the string searched and for the second the results.
+    console.log(string, results)
+  }
+
+  const handleOnHover = (result) => {
+    // the item hovered
+    console.log(result)
+  }
+
+  const handleOnSelect = (item) => {
+    // the item selected
+    console.log(item)
+  }
+
+  const handleOnFocus = () => {
+    console.log('Focused')
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div style={{ width: 400 }}>
+          <ReactSearchAutocomplete
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+          />
         </div>
-    );
+      </header>
+    </div>
+  )
 };
 
 export default SchemaSearch
