b317fc9bfc7d337030c7200482566102d7b665e8
Kall_MeKami <hopefelixn@gmail.com>
2021-06-03 10:18:58 -0400

    Janky Schema Search

--- src/components/SchemaDisplayContainer.js
+++ src/components/SchemaDisplayContainer.js
@@ -3,7 +3,7 @@ stateful functional container component responsible for rendering
 as many SchemaDisplay components as the uploaded Schema require
 */ 
 
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import SchemaDisplay from './SchemaDisplay';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
@@ -17,14 +17,16 @@ const jsonSchema = makeJsonSchema();
 
 const SchemaDisplayContainer = () => {
     // list of Schema to be rendered by SchemaDisplay
+    const [focus, setFocus] = useState(0);
     const [schemaList, setSchemaList] = useState([jsonSchema]);
+    //const schemaRef = useRef(nul;)
     const items = []
-    const refs = schemaList[0].reduce((acc, value) => {
-        //console.log(value)
-        acc[value.name] = React.createRef();
-        return acc;
-      }, {});
-      console.log(refs)
+    // const refs = schemaList[0].reduce((acc, value) => {
+    //     //console.log(value)
+    //     acc[value.name] = React.createRef();
+    //     return acc;
+    //   }, {});
+     // console.log(refs)
     for(let i = 0; i < schemaList[0].length; i++){
       items.push(
         {id: i,
@@ -32,39 +34,56 @@ const SchemaDisplayContainer = () => {
         key: i
         });
     }
-    const handleOnSearch = (string, results) => {
+    const handleOnSearch = (string, schema) => {
         // onSearch will have as the first callback parameter
         // the string searched and for the second the results.
-        console.log(string, results)
+        if(string.toLowerCase() === 'show all'){
+          console.log('Triggered')
+          setFocus(0)
+        }
+
+        console.log('serach', string, schema)
       }
     
-      const handleOnHover = (result) => {
+      const handleOnHover = (schema) => {
         // the item hovered
-        console.log(result)
+        console.log(schema)
+        //setFocus(schema.id)
       }
     
       const handleOnSelect = (schema) => {
         // the item selected
-        console.log('Selected', schema.name)
-        // document.getElementById(schema.name).scrollIntoView({
-        //     behavior: 'smooth',
-        //     block: 'start',
-        //   })
-        
+        //console.log('Selected', schema.id)
+        // scrollIntoView({ behavior: 'smooth' })
+        setFocus(schema.id)
       }
     
       const handleOnFocus = () => {
         console.log('Focused')
       }
-      
-      const display = jsonSchema.map(schema => {
-        refs[schema.name].current = schema.name;
-        return <SchemaDisplay
-        id={schema.name} 
-        key={schema.name} 
-        schemaName={schema.name}
-        schemaFields={schema.fields}/>
-    })
+      let display = {}
+      if(!focus){
+        display = jsonSchema.map(schema => {
+          //refs[schema.name].current = schema.name;
+          return <SchemaDisplay
+          //ref={schemaRef}
+          id={schema.name} 
+          key={schema.name} 
+          schemaName={schema.name}
+          schemaFields={schema.fields}/>
+        })
+      } else {
+        // console.log('Testing', schemaList[98])
+        // console.log('Focus Value', focus)
+        // console.log('The Schema', schemaList)
+        // console.log('The Focus', schemaList[focus])
+         display = <SchemaDisplay
+          id={schemaList[0][focus].name} 
+          key={schemaList[0][focus].name} 
+          schemaName={schemaList[0][focus].name}
+          schemaFields={schemaList[0][focus].fields}
+          />
+      };
 
     return (
         <Container fluid>
@@ -75,6 +94,7 @@ const SchemaDisplayContainer = () => {
             onHover={handleOnHover}
             onSelect={handleOnSelect}
             onFocus={handleOnFocus}
+            //onChange={handleOnChange}
             autoFocus
           />
             <InputGqlSchema className='mb-1' />
