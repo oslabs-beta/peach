d089807895a2ad4f299a2b0f1f57e14375e9a516
Kall_MeKami <hopefelixn@gmail.com>
2021-06-03 02:10:31 -0400

    Update SchemaDisplayContainer.js

--- src/components/SchemaDisplayContainer.js
+++ src/components/SchemaDisplayContainer.js
@@ -10,6 +10,7 @@ import Button from 'react-bootstrap/Button';
 import makeJsonSchema from '../relay/makeJsonSchema';
 import SchemaSearch from './SchemaSearch';
 import InputGqlSchema from './InputGqlSchema';
+import { ReactSearchAutocomplete } from 'react-search-autocomplete'
 
 // calls makeJsonSchema to generate a js array of objects we can render from schema.graphql
 const jsonSchema = makeJsonSchema(); 
@@ -17,19 +18,69 @@ const jsonSchema = makeJsonSchema();
 const SchemaDisplayContainer = () => {
     // list of Schema to be rendered by SchemaDisplay
     const [schemaList, setSchemaList] = useState([jsonSchema]);
+    const items = []
+    const refs = schemaList[0].reduce((acc, value) => {
+        //console.log(value)
+        acc[value.name] = React.createRef();
+        return acc;
+      }, {});
+      console.log(refs)
+    for(let i = 0; i < schemaList[0].length; i++){
+      items.push(
+        {id: i,
+        name: schemaList[0][i].name,
+        key: i
+        });
+    }
+    const handleOnSearch = (string, results) => {
+        // onSearch will have as the first callback parameter
+        // the string searched and for the second the results.
+        console.log(string, results)
+      }
+    
+      const handleOnHover = (result) => {
+        // the item hovered
+        console.log(result)
+      }
+    
+      const handleOnSelect = (schema) => {
+        // the item selected
+        console.log('Selected', schema.name)
+        // document.getElementById(schema.name).scrollIntoView({
+        //     behavior: 'smooth',
+        //     block: 'start',
+        //   })
+        
+      }
+    
+      const handleOnFocus = () => {
+        console.log('Focused')
+      }
+      
+      const display = jsonSchema.map(schema => {
+        refs[schema.name].current = schema.name;
+        return <SchemaDisplay
+        id={schema.name} 
+        key={schema.name} 
+        schemaName={schema.name}
+        schemaFields={schema.fields}/>
+    })
 
     return (
         <Container fluid>
-            <SchemaSearch schemaList={schemaList[0]}/>
+            <h1>Schema Search</h1>
+            <ReactSearchAutocomplete
+            items={items}
+            onSearch={handleOnSearch}
+            onHover={handleOnHover}
+            onSelect={handleOnSelect}
+            onFocus={handleOnFocus}
+            autoFocus
+          />
             <InputGqlSchema className='mb-1' />
             <div id="schema-display-container"  className='mb-4'>
                 <ul style={{overflow: scroll}}>
-                    {jsonSchema.map(schema => {
-                        return <SchemaDisplay 
-                        key={schema.name} 
-                        schemaName={schema.name}
-                        schemaFields={schema.fields}/>
-                    })}
+                    {display}
                 </ul>
                 <div className="_inputButton"></div>
             </div>
