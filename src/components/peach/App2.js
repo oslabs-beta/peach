40acb4c82c8f11947d3476e3a65fd35f9d412717
Graham Albachten <albachteng@gmail.com>
2021-06-12 14:12:35 -0500

    new StoreDisplay component expands as you build more queries and allows you to save a snapshot to restore later

--- src/components/peach/App2.js
+++ src/components/peach/App2.js
@@ -14,6 +14,7 @@ import QuerySelector from '../QuerySelector';
 import { useQueryLoader } from 'react-relay';
 import writtenQuery from '../../relay/__generated__/writtenQuery.graphql'
 import ResponseDisplay from '../ResponseDisplay';
+import StoreDisplay from '../StoreDisplay';
 
 const electron = window.require('electron');
 const {shell} = window.require('electron');
@@ -47,6 +48,10 @@ const App2 = () =>{
               </Card>	
               <Card className='_storeDisplay'>
                 <h5>Store Display</h5>
+                <StoreDisplay
+                  queryToLoad={queryToLoad}
+                  variables={variables}
+                />
               </Card>	
             </Col>
           </Row>
