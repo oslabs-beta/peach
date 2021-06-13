a5063376792611d5fd8e5fb0fee8dcdfe93959e0
Graham Albachten <albachteng@gmail.com>
2021-06-13 10:49:16 -0500

    add requiresAuth boolean property and auth default to null

--- src/relay/gqlEndpoint.js
+++ src/relay/gqlEndpoint.js
@@ -4,10 +4,16 @@ tiny module handles updating of graphql endpoint url, imported in fetchGraphQL
 
 const gqlEndpoint = {};
 gqlEndpoint.url = 'https://graphql.anilist.co';
-gqlEndpoint.setUrl = (newUrl) => {
+gqlEndpoint.setUrl = (newUrl, auth) => {
     if (typeof newUrl === 'string') {
         gqlEndpoint.url = newUrl;
     }
+    if (auth) {
+        gqlEndpoint.requiresAuth = true;
+        gqlEndpoint.auth = auth;
+    }
 };
+gqlEndpoint.requiresAuth = false;
+gqlEndpoint.auth = null;
 
 export default gqlEndpoint;
